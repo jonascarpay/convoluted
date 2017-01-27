35b00b887030942964beae4dfff7c48708faaf00
Jonas Carpay <jonascarpay@gmail.com>
2017-01-27 21:57:11 +0100

    MultiSoftMax function and tests

--- src/Volume.hs
+++ src/Volume.hs
@@ -2,22 +2,18 @@
                 -funfolding-use-threshold1000 -funfolding-keeness-factor1000
                 -fllvm -optlo-O3 #-}
 
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Volume where
 
 import Core
+import Data.List (foldl')
 import Data.Singletons.TypeLits
 import Data.Singletons.Prelude.Num
 import Data.Array.Repa
-import Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed as U
 
 -- | Volume and Vector hold data that is transferred betweeen layers.
 --   For now, these both contain multiple rows of the same data, i.e.
@@ -30,7 +26,16 @@ newtype Weights (s :: Size)                  = Wts (Array U DIM4 Double)
 newtype Vector  (s :: Nat) (t :: VectorType) = Vec (Array U DIM2 Double)
 
 softMax :: U.Vector Double -> U.Vector Double
-softMax xs = U.map (/expSum) exps
+softMax !xs = U.map (/expSum) exps
   where xMax   = U.maximum xs
         exps   = U.map (\x -> exp $ x - xMax) xs
         expSum = U.sum exps
+
+-- | Apply softmax to slices of the vector of lengths indicated by the list
+multiSoftMax :: [Int] -> U.Vector Double -> U.Vector Double
+multiSoftMax ls xs = U.concat $ sms (cycle ls) xs
+  where
+    sms (l:ls) xs | U.null xs = []
+                  | otherwise = let (h,t) = U.splitAt l xs
+                                 in softMax h : sms ls t
+
--- test/VolumeSpec.hs
+++ test/VolumeSpec.hs
@@ -9,12 +9,22 @@ import Test.QuickCheck
 newtype ArbUnboxed = ArbUnboxed (U.Vector Double) deriving (Eq, Show)
 
 instance Arbitrary ArbUnboxed where
-  arbitrary = do Positive (Small len) <- arbitrary
+  arbitrary = do Positive len <- arbitrary
                  elems <- vector len
                  return $! ArbUnboxed . fromList $ elems
 
 -- The softMax of a vector has sum one
-prop_softMax_sum_one (ArbUnboxed xs) = U.sum (softMax xs) `approx3` 1
+prop_softMax_sum_one          (ArbUnboxed xs) = U.sum (softMax xs) `approx3` 1
+-- applying softMax does not change the vector length
+prop_softMax_length_invariant (ArbUnboxed xs) = U.length xs == U.length (softMax xs)
+
+-- If the segment length is equal to the vector length, multiSoftMax
+-- is equivalent to regular softMax
+prop_multiSoftMax_single           (ArbUnboxed xs) =
+  softMax xs == multiSoftMax [U.length xs] xs
+prop_multiSoftMax_length_invariant (ArbUnboxed xs) =
+  U.length xs == U.length (multiSoftMax [1] xs)
+
 
 approx :: Double -> Double -> Double -> Bool
 approx x y epsilon = abs (x - y) < epsilon
