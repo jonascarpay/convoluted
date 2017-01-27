c91558e1efae0fc91c6d6514d1b42b24babfaa2d
Jonas Carpay <jonascarpay@gmail.com>
2017-01-27 20:25:55 +0100

    MultiSoftMax data type

---
+++ src/Layers.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Layers where
+
+import Network
+import Data.Singletons.Prelude
+import Data.Singletons.TypeLits
+import Data.Singletons.Prelude.List
+
+data MultiSoftMax (cs :: [Nat]) = MultiSoftMax
+
+instance (Sum cs ~ Prod s) => OutputLayer (MultiSoftMax cs) s where
+  -- runOutput :: Monad m => MultiSoftMax -> Volume i -> (Vector o Probs)
+  runOutput = undefined
+
+class KnownNats (a :: [Nat]) where
+  natVals :: p a -> [Integer]
+
+instance KnownNats '[] where
+  natVals _ = []
+
+instance (KnownNats xs, KnownNat x) => KnownNats (x ': xs) where
+  natVals _ = natVal (undefined :: p x) : natVals (undefined :: p xs)
--- src/Network.hs
+++ src/Network.hs
@@ -30,9 +30,9 @@ class Updatable l => Layer l (i :: Size) (o :: Size) where
   runForward   :: Monad m => l -> Volume i -> m (Volume o)
   runBackwards :: Monad m => l -> Volume i -> Volume o -> m (Gradient l, Volume o)
 
-class (Prod i ~ Sum cs) => OutputLayer l (i :: Size) (cs :: [Nat]) where
-  runOutput :: Monad m => l -> Volume i -> m (Vector cs Probs)
-  getError  :: Monad m => l -> Vector cs OneHot -> m (Volume i)
+class OutputLayer l (i :: Size) where
+  runOutput :: Monad m => l -> Volume i -> m (Vector o Probs)
+  getError  :: Monad m => l -> Vector o OneHot -> m (Volume i)
 
 data Size = S Nat Nat Nat
 
@@ -42,15 +42,15 @@ type instance Prod ('S a b c) = (a :* b) :* c
 data VectorType = Probs | OneHot
 
 newtype Volume (s :: Size) = Vol Int
-newtype Vector (s :: [Nat]) (t :: VectorType) = Vec Int
+newtype Vector (s :: Nat) (t :: VectorType) = Vec Int
 
-data Network (i :: Size) (ls :: [*]) (cs :: [Nat]) where
-  NNil  :: OutputLayer x i cs => !x                       -> Network i '[x]      cs
-  NCons :: Layer x i o        => !x -> !(Network o xs cs) -> Network i (x ': xs) cs
+data Network (i :: Size) (ls :: [*]) (o :: Nat) where
+  NNil  :: OutputLayer x i => !x                       -> Network i '[x]      (Prod i)
+  NCons :: Layer x i o     => !x -> !(Network o xs no) -> Network i (x ': xs) no
 
 data Gradients :: [*] -> * where
   GNil  :: Updatable x => Gradient x -> Gradients '[x]
   GCons :: Updatable x => Gradient x -> Gradients xs -> Gradients (x ': xs)
 
-class CreatableNetwork (i :: Size) (xs :: [*]) (cs :: [Nat]) where
-  randomNetwork :: MonadRandom m => m (Network i xs cs)
+class CreatableNetwork (i :: Size) (xs :: [*]) (o :: Nat) where
+  randomNetwork :: MonadRandom m => m (Network i xs o)
