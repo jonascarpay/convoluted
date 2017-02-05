7e788c631383339626a80382d20d3c1a7d94c81c
Jonas Carpay <jonascarpay@gmail.com>
2017-02-05 14:48:28 +0100

    fundeps in network module

--- src/Network.hs
+++ src/Network.hs
@@ -7,6 +7,7 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
 
 module Network where
 
@@ -28,48 +29,31 @@ class Updatable l where
   applyDelta _ _ _ = return zeroLayer
 
 -- | An instance of the Layer class is a valid layer for a neural network.
-class (Measure o, Updatable l) => Layer l (o :: SMeasure) where
+class (Measure i, Measure o, Updatable l) => Layer (i :: SMeasure) l (o :: SMeasure) | i l -> o where
 
-  type InputShape l o :: SMeasure
-
-  runForward   :: (Monad m, Measure (InputShape l o), Measure o)
+  runForward   :: (Monad m, Measure i, Measure o)
                => l
-               -> SArray U (InputShape l o) -- ^ Input data
-               -> m (SArray U o)            -- ^ Output data after passing through this layer
+               -> SArray U i     -- ^ Input data
+               -> m (SArray U o) -- ^ Output data after passing through this layer
 
-  runBackwards :: (Monad m, Measure (InputShape l o), Measure o)
+  runBackwards :: (Monad m, Measure i, Measure o)
                => l
-               -> SArray U (InputShape l o) -- ^ Input data during forward pass
-               -> SArray U o -- ^ Output data during forward pass. Note that this could be recomputed, but it seems more efficient to keep a reference around.
-               -> SArray U o -- ^ Gradient on the output data
-               -> m (Gradient l, SArray U (InputShape l o)) -- ^ Gradient on the weights, gradient on the input data
+               -> SArray U i                 -- ^ Input data during forward pass
+               -> SArray U o                 -- ^ Output data during forward pass. Note that this could be recomputed, but it seems more efficient to keep a reference around.
+               -> SArray U o                 -- ^ Gradient on the output data
+               -> m (Gradient l, SArray U i) -- ^ Gradient on the weights, gradient on the input data
 
-class Layer l o => OutputLayer l o
+class Layer i l o => OutputLayer i l o
 
-data Network (ls :: [*]) (o :: SMeasure) where
-  NNil  :: OutputLayer l o                   => l -> Network (l ': '[]) o
-  NCons :: Layer l (NetInput (Network ls o)) => l -> Network ls o -> Network (l ':  ls) o
-
-type family NetInput l :: SMeasure where
-  NetInput (Network (l ': '[]) o) = InputShape l o
-  NetInput (Network (l ':  ls) o) = InputShape  l (NetInput (Network ls o))
+data Network (i :: SMeasure) (ls :: [*]) (o :: SMeasure) where
+  NNil  :: OutputLayer i l o => l -> Network i (l ': '[]) o'
+  NCons :: Layer i l o       => l -> Network o ls o' -> Network i (l ':  ls) o'
 
 data Gradients :: [*] -> * where
   GNil  :: Updatable x => Gradient x                 -> Gradients (x ': '[])
   GCons :: Updatable x => Gradient x -> Gradients xs -> Gradients (x ':  xs)
 
-class CreatableNetwork (ls :: [*]) (o :: SMeasure) where
-  randomNetwork :: Int -> Network ls o
-  zeroNetwork   :: Network ls o
-
-instance OutputLayer l o => CreatableNetwork (l ': '[]) o where
-  zeroNetwork        = NNil zeroLayer
-  randomNetwork seed = NNil (randomLayer seed)
-
-instance ( Layer l (NetInput (Network (l' ': ls) o))
-         , CreatableNetwork (l' ': ls) o
-         ) => CreatableNetwork (l ': l' ': ls) o where
-
-  zeroNetwork = zeroLayer `NCons` zeroNetwork
-  randomNetwork seed = randomLayer seed `NCons` randomNetwork (seed^(9 :: Int))
+class CreatableNetwork (i :: SMeasure) (ls :: [*]) (o :: SMeasure) where
+  randomNetwork :: Int -> Network i ls o
+  zeroNetwork   :: Network i ls o
 
