75540773f479137ea48a401d64c67999a8d6b30f
Jonas Carpay <jonascarpay@gmail.com>
2017-02-13 12:05:37 +0100

    ReLU update

--- src/Layers/ReLU.hs
+++ src/Layers/ReLU.hs
@@ -8,6 +8,7 @@ module Layers.ReLU where
 
 import Network
 import Static
+import Data.Serialize
 
 data ReLU = ReLU
 
@@ -15,8 +16,13 @@ instance Updatable ReLU where
   type Gradient ReLU = ReLU
   zeroLayer = ReLU
 
-instance Measure s => Layer s ReLU s where
+instance Measure s => Layer s ReLU where
+  type LOutput i ReLU = i
   runForward _ x = sComputeP$ sMap (max 0) x
   runBackwards _ _ y dy =
     do dx <- sComputeP$ sZipWith (\x t -> if t > 0 then x else 0) dy y
        return (ReLU, dx)
+
+instance Serialize ReLU where
+  put _ = return ()
+  get = return ReLU
