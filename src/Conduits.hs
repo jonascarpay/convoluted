1129abfb99c30fc0cd4440b0f0bdaff6e579b82c
Jonas Carpay <jonascarpay@gmail.com>
2017-02-14 18:43:56 +0100

    Default conduits

---
+++ src/Conduits.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Conduits where
+
+import Network
+import Conduit
+import Static
+import Runners
+import Control.Applicative
+
+trainC :: forall m i ls.
+          ( MonadIO m
+          , CreatableNetwork i ls
+          )
+       => LearningParameters
+       -> Conduit ( SArray U i
+                  , SArray U (NOutput (Network i ls))
+                  ) m (Network i ls)
+trainC params = go (randomNetwork 9 :: Network i ls)
+  where go net = do mxy <- await
+                    case mxy of
+                      Just (x, y) -> do (net', (pct, dtl)) <- trainOnce net params x y
+                                        liftIO . putStrLn$ show pct ++ '\t':show dtl
+                                        yield net'
+                                        go net'
+                      Nothing -> return ()
+
+combineXY :: Monad m
+          => Source m x
+          -> Source m y
+          -> Source m (x, y)
+combineXY sx sy = getZipSource $ liftA2 (,) (ZipSource sx) (ZipSource sy)
