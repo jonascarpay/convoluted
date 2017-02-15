a2c4f4586d3a5ec39f664029e08e8b8c5e7eec04
Jonas Carpay <jonascarpay@gmail.com>
2017-02-15 18:34:02 +0100

    Rework labels

--- src/Network/Label.hs
+++ src/Network/Label.hs
@@ -1,58 +1,88 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 
-module Network.Label where
+module Network.Label
+  ( singleton
+  , hcat
+  , vcat
+  , toArray
+  , fromArray
+  , LabelComposite
+  , LabelSingle
+  ) where
 
 import Static
 import Data.Singletons
 import Data.Singletons.TypeLits
 import Data.Singletons.Prelude.Num
 import Data.Singletons.Prelude.List
-import Data.Vector.Unboxed as U
-import Data.Vector.Unboxed.Mutable as U
 import Control.Monad.ST.Strict
+import qualified Data.Vector.Unboxed         as U
+import qualified Data.Vector.Unboxed.Mutable as UM
+import qualified Data.Sequence               as Sq
+import qualified Data.Array.Repa as R
 
-newtype Label (c :: Nat) = Label Int
+type LabelSingle (c :: Nat) = LabelComposite 1 '[c]
+newtype LabelComposite (rows :: Nat) (cols :: [Nat]) = Label (Sq.Seq Int)
 
-data LabelSeq (cs :: [Nat]) where
-  LNil  :: LabelSeq '[]
-  LCons :: KnownNat c => Label c -> LabelSeq cs -> LabelSeq (c ': cs)
+instance Show (LabelComposite r c) where
+  show (Label seq) = "Label (" ++ show seq ++ ")"
 
-data LabelBatch (n :: Nat) (cs :: [Nat]) where
-  LBNil :: LabelBatch 0 cs
-  LBCons :: KnownNat n => LabelSeq cs -> LabelBatch n cs -> LabelBatch (n :+ 1) cs
+instance Eq (LabelComposite r c) where
+  (Label s1) == (Label s2) = s1 == s2
 
-toSArray :: forall cs n.
-  ( KnownNat n
-  , KnownNat (Sum cs)
-  , KnownNat (Size (ZZ ::. n ::. Sum cs))
-  , SingI cs
-  )
-  => LabelBatch n cs
-  -> SArray U (ZZ ::. n ::. Sum cs)
-toSArray = sFromUnboxed . toVector
+singleton :: forall c. KnownNat c => Int -> LabelSingle c
+singleton x
+  | x >= c = error $ "Label value " ++ show x ++ " too high, max value is " ++ show c
+  | x < 0  = error "Label value too low"
+  | otherwise = Label . Sq.singleton $ x
+  where c = fromInteger$ natVal (Proxy :: Proxy c)
 
-class ToVector l where
-  toVector :: l -> Vector Double
+instance KnownNat c => Num (LabelSingle c) where
+  Label (Sq.viewl -> x1 Sq.:< _) + Label (Sq.viewl -> x2 Sq.:< _) = singleton$ x1 + x2
+  Label (Sq.viewl -> x1 Sq.:< _) - Label (Sq.viewl -> x2 Sq.:< _) = singleton$ x1 - x2
+  Label (Sq.viewl -> x1 Sq.:< _) * Label (Sq.viewl -> x2 Sq.:< _) = singleton$ x1 * x2
+  abs (Label (Sq.viewl -> x Sq.:< _)) = singleton$ abs x
+  signum = undefined
+  fromInteger = singleton . fromInteger
 
-instance KnownNat c => ToVector (Label c) where
-  toVector (Label n) = runST$ do let len = fromInteger$ natVal (Proxy :: Proxy c)
-                                 vec <- new len
-                                 write vec n 1
-                                 unsafeFreeze vec
+hcat :: LabelComposite 1 c1 -> LabelComposite 1 c2 -> LabelComposite 1 (c1 :++ c2)
+hcat (Label s1) (Label s2) = Label$ s1 Sq.>< s2
 
-instance ToVector (LabelSeq cs) where
-  toVector ls = U.concat $ go ls
-    where
-      go :: LabelSeq cs' -> [Vector Double]
-      go LNil = []
-      go (l `LCons` ls) = toVector l : go ls
+vcat :: LabelComposite r1 cols -> LabelComposite r2 cols -> LabelComposite (r1 :+ r2) cols
+vcat (Label s1) (Label s2) = Label$ s1 Sq.>< s2
 
-instance ToVector (LabelBatch n cs) where
-  toVector lbs = U.concat $ go lbs
-    where
-      go :: LabelBatch n' cs' -> [Vector Double]
-      go LBNil = []
-      go (lb `LBCons` lbs) = toVector lb : go lbs
+toArray :: forall r cs. ( KnownNat r
+                        , SingI cs
+                        , KnownNat (Sum cs)
+                        , KnownNat (Sum cs :* r)
+                        )
+        => LabelComposite r cs
+        -> SArray U (ZZ ::. r ::. Sum cs)
+toArray (Label seq) = sFromUnboxed vec
+  where
+    cs = fromSing (sing :: Sing cs)
+    size = fromInteger $ natVal (Proxy :: Proxy (Sum cs :* r))
+
+    vec = runST$ do v <- UM.new size
+                    writeLabels v 0 (cycle cs) (Sq.viewl seq)
+                    U.unsafeFreeze v
+
+    writeLabels _ _      _       Sq.EmptyL     = return ()
+    writeLabels v offset ~(c:cs) ~(n Sq.:< ns) =
+      do UM.write v (offset + n) 1
+         writeLabels v (offset + fromInteger c) cs (Sq.viewl ns)
+
+fromArray :: forall r cs . SingI cs => SArray U (ZZ ::. r ::. Sum cs) -> LabelComposite r cs
+fromArray (SArray (R.toUnboxed -> vec)) = Label$ go vec (cycle cs)
+  where
+    cs = fromSing (sing :: Sing cs)
+    go vec ~(c:cs) =
+      case U.splitAt (fromInteger c) vec of
+        (h, t)
+          | U.null h  -> Sq.empty
+          | otherwise -> U.maxIndex h Sq.<| go t cs
